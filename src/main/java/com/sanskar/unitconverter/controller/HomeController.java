a8f081f20479b2fe006b2b6b19a81e0e9c2efc28
Sanskar Soni <97246753+Sannutan@users.noreply.github.com>
2026-07-28 17:22:31 +0530

    Add length conversion feature using Spring MVC and Thymeleaf

--- src/main/java/com/sanskar/unitconverter/controller/HomeController.java
+++ src/main/java/com/sanskar/unitconverter/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.sanskar.unitconverter.controller;
 
+import com.sanskar.unitconverter.model.LengthConversionRequest;
 import com.sanskar.unitconverter.service.UnitConversionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
 public class HomeController {
@@ -16,8 +21,23 @@ public class HomeController {
         return "index";
     }
     @GetMapping("/length")
-    public String length() {
+    public String length(Model model) {
+        model.addAttribute("request", new LengthConversionRequest());
         return "length";
     }
 
+    @PostMapping("/length")
+    public String convertLength(
+            @ModelAttribute("request") LengthConversionRequest request,
+            Model model) {
+            double result = unitConversionService.convertingLength(
+                    request.getValue(),
+                    request.getFrom(),
+                    request.getTo());
+
+            model.addAttribute("request", request);
+            model.addAttribute("result", result);
+            return "length";
+
+    }
 }
--- src/main/java/com/sanskar/unitconverter/service/UnitConversionService.java
+++ src/main/java/com/sanskar/unitconverter/service/UnitConversionService.java
@@ -5,4 +5,74 @@ import org.springframework.stereotype.Service;
 @Service
 public class UnitConversionService {
 
+    public double convertingLength(double value, String from, String to){
+
+        double metres = convertToMetres(value, from);
+        return convertFromMetres(metres, to);
+    }
+
+    private double convertToMetres(double value, String unit) {
+        switch (unit) {
+            case "mm":
+                return value / 1000;
+
+            case "cm":
+                return value / 100;
+
+            case "m":
+                return value;
+
+            case "km":
+                return value * 1000;
+
+            case "inch":
+                return value * 0.0254;
+
+            case "foot":
+                return value * 0.3048;
+
+            case "yard":
+                return value * 0.9144;
+
+            case "mile":
+                return value * 1609.344;
+
+            default:
+                throw new IllegalArgumentException("Invalid unit");
+        }
+    }
+
+    private double convertFromMetres(double metres, String unit){
+
+        switch (unit){
+            case "mm":
+            return metres * 1000;
+
+            case "cm":
+                return metres * 100;
+
+            case "m":
+                return metres;
+
+            case "km":
+                return metres / 1000;
+
+            case "inch":
+                return metres / 0.0254;
+
+            case "foot":
+                return metres / 0.3048;
+
+            case "yard":
+                return metres / 0.9144;
+
+            case "mile":
+                return metres / 1609.344;
+
+            default:
+                throw new IllegalArgumentException("Invalid Unit");
+        }
+    }
+
+
 }
